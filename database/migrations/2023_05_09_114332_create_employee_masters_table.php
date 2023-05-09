e36232557406d3e00fdc37ebe496274d7360871b
kashifalidevstudio <kashifali@devstudio.us>
2023-05-09 16:46:16 +0500

    create sql database

---
+++ database/migrations/2023_05_09_114332_create_employee_masters_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('employee_masters', function (Blueprint $table) {
+            $table->id();
+            $table->string("first_name")->nullable();
+            $table->string("last_name")->nullable();
+            $table->string("email")->nullable();
+            $table->string("role")->nullable();
+            $table->string("password")->nullable();
+            $table->string("entered_date")->nullable();
+            $table->integer("is_active")->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('employee_masters');
+    }
+};
